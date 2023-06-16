f0676e6755caa1184cadfce3d0893625bb43fe14
Huzaifa Ghouri <120574411+HuzaifaGhouri08@users.noreply.github.com>
2023-06-16 18:46:35 +0500

    Variable Declaration and Initialization

---
+++ Example2.js
@@ -0,0 +1,12 @@
+console.log(x); // Undefined
+var x = 10;
+
+console.log(y); // Cannot access 'y' before initialization
+let y = 10;
+
+// in real
+let z;
+console.log(z); // Undefined
+
+z = 20;
+console.log(z); // 20
